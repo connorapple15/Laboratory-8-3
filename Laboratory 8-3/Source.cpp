fe99fc397ee776b1a38675b34ff5f810232294ef
Connor Apple <c-apple@onu.edu>
2024-10-31 09:17:15 -0400

    Completed convertRomanToInt Function

--- Laboratory 8-3/Source.cpp
+++ Laboratory 8-3/Source.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int romanCharValue(char r);
+int convertRomanToInt(const string& s);
 
 int main() {
     cout << "Testing romanCharValue function:\n";
@@ -14,6 +15,9 @@ int main() {
     cout << "C = " << romanCharValue('C') << endl; 
     cout << "D = " << romanCharValue('D') << endl; 
     cout << "M = " << romanCharValue('M') << endl; 
+    ;
+    cout << "MCMLXII = " << convertRomanToInt("MCMLXII") << endl; 
+    cout << "MDCCCLXXXVIII = " << convertRomanToInt("MDCCCLXXXVIII") << endl; 
 
     return 0;
 }
@@ -29,4 +33,18 @@ int romanCharValue(char r) {
     case 'M': return 1000;
     default: return 0; 
     }
+}
+int convertRomanToInt(const string& s) {
+    int total = 0;
+    int length = s.length();
+
+    for (int i = 0; i < length; i++) {
+        if (i + 1 == length || romanCharValue(s[i]) >= romanCharValue(s[i + 1])) {
+            total += romanCharValue(s[i]);
+        }
+        else {
+            total -= romanCharValue(s[i]);
+        }
+    }
+    return total;
 }
